48a1cfc0c6e053ea71c3e229fd9a8601fbde8154
kijoyoo <youkijo@daum.net>
2020-03-17 18:39:21 +0900

    make User Query  and make hashcode

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT : number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT : string = "/playground";
 const GRAPHQL_ENDPOINT : string = "/graphql"
 
-const appOptions : Options ={
+const appOptions : Options = {
     port: PORT,
     playground: PLAYGROUND_ENDPOINT,
     endpoint: GRAPHQL_ENDPOINT
