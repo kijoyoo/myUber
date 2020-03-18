70647817539a44b2d7e170da4fe0e6997c766268
kijoyoo <youkijo@daum.net>
2020-03-18 18:49:51 +0900

    create key and make Place.graphql

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,11 +1,14 @@
-import {Column, Entity, BaseEntity,PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from "typeorm"
+import {Column, Entity, BaseEntity,PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm"
 import { verificationTarget } from "src/types/types";
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
 
 @Entity()
 class Verification extends BaseEntity{
     @PrimaryGeneratedColumn() id: number
 
-    @Column({type:"text", enum: ["PHONE","EMAIL"]})
+    @Column({type:"text", enum: [PHONE,EMAIL] })
     target: verificationTarget;
 
     @Column({type:"text"})
@@ -19,6 +22,16 @@ class Verification extends BaseEntity{
 
     @CreateDateColumn() createAt: string;
     @UpdateDateColumn() updateAt: string;
+
+    @BeforeInsert()
+    createKey(): void {
+        if(this.key === PHONE){
+            this.key = Math.floor(Math.random()*10000).toString();
+        }else if(this.key === EMAIL){
+            this.key = Math.random().toString(36).substr(2);
+        }
+    }
+
 }
 
 export default Verification;
