52a7eb53d9e38295513fbca69388791bbeb5c6d4
kijoyoo <youkijo@daum.net>
2020-03-18 15:17:00 +0900

    add Verification gql and tranfer to typescript

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,15 +1,38 @@
-export const typeDefs = ["type sayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype Query {\n  sayHello(name: String!): sayHelloResponse!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  email: String!\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  fullName: String\n  createAt: String!\n  updateAt: String\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayHello: sayHelloResponse;
+  user: User | null;
 }
 
-export interface SayHelloQueryArgs {
-  name: string;
+export interface User {
+  id: number;
+  email: string;
+  verifiedEmail: boolean;
+  firstName: string;
+  lastName: string;
+  age: number | null;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLng: number | null;
+  lastLat: number | null;
+  lastOrientation: number | null;
+  fullName: string | null;
+  createAt: string;
+  updateAt: string | null;
 }
 
-export interface sayHelloResponse {
-  text: string;
-  error: boolean;
+export interface Verification {
+  id: number;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
